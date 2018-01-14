5981a987e4267d3cfa5ebf0683deaf789e1cb716
Artur Popov <Artur.Popov@coopersoftware.co.uk>
2018-01-14 14:47:18 +0000

    initial commit

---
+++ HelloTriangle/main.cpp
@@ -0,0 +1,58 @@
+#define GLFW_INCLUDE_VULKAN
+#include "..\..\glfw-3.2.1.bin.WIN64\include\GLFW\glfw3.h"
+//#include "..\..\glfw-3.2.1.bin.WIN64\include\GLFW\glfw3native.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "..\..\glm-0.9.9-a2\glm\vec3.hpp"
+#include "..\..\glm-0.9.9-a2\glm\mat4x4.hpp"
+
+#include <stdexcept>
+#include <functional>
+#include <iostream>
+
+class HelloTriangleApplication {
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	GLFWwindow * window;
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+	void initWindow() {
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Hello Triangle", nullptr, nullptr);
+	}
+
+	void initVulkan() {
+
+	}
+
+	void mainLoop() {
+
+	}
+
+	void cleanup() {
+
+	}
+};
+
+int main() {
+	HelloTriangleApplication app;
+
+	try {
+		app.run();
+	}
+	catch (const std::runtime_error& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
