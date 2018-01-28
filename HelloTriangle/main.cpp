446d1cf7c295ccb7664ce1b5bb26c770e8487ea4
Artur Popov <Artur.Popov@coopersoftware.co.uk>
2018-01-28 01:36:39 +0000

    selecting gpu based on isDevice criteria. Currently set to discrete gpu & geometry shader

--- HelloTriangle/main.cpp
+++ HelloTriangle/main.cpp
@@ -207,9 +207,49 @@ private:
 		}
 	}
 
+	void pickPhysicalDevice()
+	{
+		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+		if(deviceCount == 0)
+		{
+			throw std::runtime_error("failed to find GPUs with Vulkan support");
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		for (const auto& device : devices)
+		{
+			if(isDeviecSuitable(device))
+			{
+				physicalDevice = device;
+				break;
+			}
+		}
+
+		if(physicalDevice == VK_NULL_HANDLE)
+		{
+			throw std::runtime_error("failed to find suitable GPU!");
+		}
+	}
+
+	bool isDeviecSuitable(VkPhysicalDevice device)
+	{
+		VkPhysicalDeviceProperties deviceProperties;
+		VkPhysicalDeviceFeatures deviceFeatures;
+		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU 
+			&& deviceFeatures.geometryShader;
+	}
+
 	void initVulkan() {
 		createInstance();
 		setupDebugCallback();
+		pickPhysicalDevice();
 	}
 
 	void mainLoop() {
